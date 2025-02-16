22a4fe742a8694c1e8f01b43d7fa76517df72dff
Borgwarg <99420548+Borgwarg@users.noreply.github.com>
2025-02-16 10:36:41 +0100

    Create sixteen columns using java

---
+++ java.js
@@ -0,0 +1,11 @@
+let gridcontainer = document.querySelector("#gridcontainer");
+
+function createGrid(size) {
+    for(let i = 0; i < size; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        gridcontainer.appendChild(row);
+    }
+}
+
+createGrid(16);
