057429ebb45742cb0a3b9193cca656bd315a1292
Borgwarg <99420548+Borgwarg@users.noreply.github.com>
2025-02-16 11:03:54 +0100

    create 16 rows using flexbox

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
 let gridcontainer = document.querySelector("#gridcontainer");
 
 function createGrid(size) {
-    for(let i = 0; i < size; i++) {
+    for (let i = 0; i < size; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
         gridcontainer.appendChild(row);
