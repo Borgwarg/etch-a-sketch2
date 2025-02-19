64ce5b5a087244747be181bde47e8e29b32034af
Borgwarg <99420548+Borgwarg@users.noreply.github.com>
2025-02-19 13:48:49 +0100

    Move declaration of gridsquares variable inside draw function and inside createMouseEvents function

--- java.js
+++ java.js
@@ -12,8 +12,46 @@ function getSize() {
 }
 
 
+
+let drawing = false;
+
+function createMouseEvents() {
+    const gridsquares = document.querySelectorAll(".gridsquare");
+
+    gridsquares.forEach(gridsquare => {
+        gridsquare.addEventListener("mousedown", () => {
+            gridsquare.style.backgroundColor = "red";
+            drawing = true;
+            draw();
+        });
+        gridsquare.addEventListener("mouseup", () => {
+            drawing = false;
+        })
+    });
+};
+
+function draw() { 
+    const gridsquares = document.querySelectorAll(".gridsquare");
+
+    gridsquares.forEach(gridsquare => {
+        gridsquare.addEventListener("mouseenter", () => {
+            if (drawing === true) {
+                gridsquare.style.backgroundColor = "red"
+            } else {
+                gridsquare.style.nackgroundColor = "white"
+            }
+        });
+    });
+}
+
+createGridButton = document.querySelector(".createGrid");
+createGridButton.addEventListener("click", () => {
+    createGrid(getSize());
+});
+
 function createGrid(size) {
     gridcontainer.innerHTML = "";
+
     for (let i = 0; i < size; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
@@ -24,44 +62,11 @@ function createGrid(size) {
         gridsquare.classList.add("gridsquare")
         row.appendChild(gridsquare);
         gridsquare.id = "gridsquare";
-        }
-    }
+        };
+    };
 
-}
+    createMouseEvents();
+
+};
 
 createGrid(50);
-
-
-
-
-const gridsquares = document.querySelectorAll(".gridsquare");
-let drawing = false;
-
-gridsquares.forEach(gridsquare => {
-    gridsquare.addEventListener("mousedown", () => {
-        gridsquare.style.backgroundColor = "red";
-        drawing = true;
-        draw();
-    });
-    gridsquare.addEventListener("mouseup", () => {
-        drawing = false;
-    })
-});
-
-
-function draw() { 
-    gridsquares.forEach(gridsquare => {
-        gridsquare.addEventListener("mouseenter", () => {
-            if (drawing === true) {
-                gridsquare.style.backgroundColor = "red"
-            } else {
-                gridsquare.style.nackgroundColor = "white"
-            }
-        })
-    })
-}
-
-createGridButton = document.querySelector(".createGrid");
-createGridButton.addEventListener("click", () => {
-    createGrid(getSize());
-});
